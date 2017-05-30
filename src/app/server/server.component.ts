c4928f49b52f938d4af05c6838805b24a98d8f31
juliusmbn <julius@mybizniche.com>
2017-05-30 14:07:40 +0800

    np 4 fundamentals experiment

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent{
+   test:string = "String Interpolation";
+
+   allowNewServer:boolean = false;
+
+   clicker = function(){
+       console.log("hello");
+   }
+}
